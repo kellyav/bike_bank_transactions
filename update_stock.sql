cd68b63f0d5ada716c608811b94864257f4954ba
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-16 13:42:03 -0400

    Update update_stock.sql

--- update_stock.sql
+++ update_stock.sql
@@ -1,30 +1,41 @@
+/* The products table does not have any constraint to check the data stored in its stock column. 
+Stock counts cannot be negative.  
+A constraint is required for the products table, so that only stocks greater than or equal to 0 are allowed.
 
-/* the products table has a constraint requiring every product name to be unique.
-
-When updating the stock, a catch block can catch any errors and avoid misreporting stock! */
-
+Trek Neko+  has a -1 stock, which causes an error when trying to add the constraint. 
+The execution will fail the first time, because there is one row where the stock equals -1. */
 
 BEGIN TRY
-    INSERT INTO products (product_name, stock, price) 
-    VALUES	('Trek Powerfly 5 - 2018', 2, 3499.99),   		
-    		('New Power K- 2018', 3, 1999.99)		
-END TRY
--- Set up the outer CATCH block
+	-- Add the constraint
+	ALTER TABLE products
+		ADD CONSTRAINT CHK_Stock CHECK (stock >= 0);
+END TRY 
+
 BEGIN CATCH
-	SELECT 'An error occurred inserting the product!';
-    -- Set up the inner TRY block
-    BEGIN TRY
-    	-- Insert the error
-    	INSERT INTO errors 
-        	VALUES ('Error inserting a product');
-    END TRY    
-    -- Set up the inner CATCH block
-    BEGIN CATCH
-    	-- Show number and message error
-    	SELECT 
-        	ERROR_LINE() AS line,	   
-			ERROR_MESSAGE() AS message; 
-    END CATCH    
+	SELECT 'An error occurred!';
 END CATCH
 
--- error (message): An error occurred inserting the product!
+/* To remedy this, we must change all negative stocks to 0. */ 
+
+SELECT 
+	stock
+CASE WHEN stock < 0 THEN 0
+ELSE stock
+END AS stock_nonneg
+
+FROM products
+
+/* As an outside user, it is not clear why the table has negative numbers for stock. 
+So as not to damage the original stock values, the new non-negative stock amounts are stored in a new column. 
+The original column (that includes negativ stock counts) can be deleted, if necessary. */
+
+-- Now, we can actually add the constraint for future use. 
+BEGIN TRY
+	-- Add the constraint
+	ALTER TABLE products
+		ADD CONSTRAINT CHK_Stock CHECK (stock_nonneg >= 0);
+END TRY 
+
+BEGIN CATCH
+	SELECT 'An error occurred!';
+END CATCH
