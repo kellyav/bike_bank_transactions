6333d6a962c5a84d37c8d51c8a1ade2ddea4fd2d
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-16 13:19:46 -0400

    Update and rename update_stock.rtf to update_stock.sql

---
+++ update_stock.sql
@@ -0,0 +1,30 @@
+
+/* the products table has a constraint requiring every product name to be unique.
+
+When updating the stock, a catch block can catch any errors and avoid misreporting stock! */
+
+
+BEGIN TRY
+    INSERT INTO products (product_name, stock, price) 
+    VALUES	('Trek Powerfly 5 - 2018', 2, 3499.99),   		
+    		('New Power K- 2018', 3, 1999.99)		
+END TRY
+-- Set up the outer CATCH block
+BEGIN CATCH
+	SELECT 'An error occurred inserting the product!';
+    -- Set up the inner TRY block
+    BEGIN TRY
+    	-- Insert the error
+    	INSERT INTO errors 
+        	VALUES ('Error inserting a product');
+    END TRY    
+    -- Set up the inner CATCH block
+    BEGIN CATCH
+    	-- Show number and message error
+    	SELECT 
+        	ERROR_LINE() AS line,	   
+			ERROR_MESSAGE() AS message; 
+    END CATCH    
+END CATCH
+
+-- error (message): An error occurred inserting the product!
